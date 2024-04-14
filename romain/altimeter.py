eae5964bdbcd1c1b1c5cdb3d4ce4339cc9601c65
RomainAstoria <164931010+RomainAstoria@users.noreply.github.com>
2024-04-14 09:16:18 +0200

    Thermometer in dark mode, altimeter prototype with atomium, eiffel tower and Burj Khalifa.

--- romain/altimeter.py
+++ romain/altimeter.py
@@ -3,36 +3,53 @@ from matplotlib import image as mpimg
 from matplotlib.patches import Rectangle
 import random
 
+# 0	    1170	-1	390	780	    -45	735
+# 400	1170	0	390	1170	-45	1125
+# 800	1170	1	390	1560	-45	1515
+# 1200	1170	2	390	1950	-45	1905	1170
+
+
 plt.gcf().set_facecolor('black')
 w = 52  # largeur du rectangle
 x = 45
 # Fonction pour ajouter une image à une position spécifique
-def add_image_with_position(img_path, position, zorder=0):
+
+# Génération d'une valeur aléatoire initiale
+random_value = random.randint(0, 1200)
+
+
+def add_image_with_position(img_path: object, position: object, zorder: object = 0) -> object:
     img = mpimg.imread(img_path)
-    plt.imshow(img, extent=(position[0], position[0] + img.shape[1], position[1], position[1] + img.shape[0]), zorder=zorder)
-    return img.shape
+    image_object = plt.imshow(img, extent=(position[0], position[0] + img.shape[1], position[1], position[1] + img.shape[0]), zorder=zorder)
+    return image_object
+
 
 # Chargement de l'image de la jauge
 img_path_background = r'media\altimeter_all.png'
-background_shape = add_image_with_position(img_path_background, (0, 0), zorder=0)
+add_image_with_position(img_path_background, (0, 0), zorder=1)
 
 # Chargement de l'image atomium color
-img_path_eiffel_tower = r'media\atomiumcolor.png'
-eiffel_tower_position = (0, 0)  # Définissez la position de la tour Eiffel
-add_image_with_position(img_path_eiffel_tower, eiffel_tower_position, zorder=3)
+img_path_atomium_color = r'media\atomiumcolor.png'
+atomium_color_position = (0, 0)  # Définissez la position de la tour Eiffel
+atomium_color = add_image_with_position(img_path_atomium_color, atomium_color_position, zorder=0)
 
 # Chargement de l'image Eiffel Tower color
 img_path_eiffel_tower = r'media\eiffeltowercolorgut.png'
 eiffel_tower_position = (0, 0)  # Définissez la position de la tour Eiffel
-add_image_with_position(img_path_eiffel_tower, eiffel_tower_position, zorder=2)
+eiffel_tower = add_image_with_position(img_path_eiffel_tower, eiffel_tower_position, zorder=0)
 
 # Chargement de l'image Burj Khalifa color
 img_path_burj_khalifa = r'media\burjkhalifacolorgut.png'
 burj_khalifa_position = (0, 0)  # Définissez la position de la tour Burj Khalifa
-add_image_with_position(img_path_burj_khalifa, burj_khalifa_position, zorder=1)
+burj_khalifa = add_image_with_position(img_path_burj_khalifa, burj_khalifa_position, zorder=0)
+
+# Chargement de l'image cansat black color
+img_path_can_black_icon = r'media\can_black_icon.png'
+can_black_icon_position = (200, 300 )  # Définissez la position
+can_black_icon = add_image_with_position(img_path_can_black_icon, can_black_icon_position, zorder=3)
 
 # Réglez la limite des axes pour s'adapter aux images
-plt.xlim(200, 1250)
+plt.xlim(0, 1250)
 plt.ylim(450, 2000)
 
 # Désactivez les axes et affichez uniquement les images
@@ -66,9 +83,6 @@ for graduation in graduations_a_afficher:
     position = graduations_positions[graduation]
     plt.text(position[0], position[1], f'{graduation}m', fontsize=8, ha='center', va='center', color='white')
 
-# Génération d'une valeur aléatoire initiale
-random_value = random.randint(0, 1200)
-
 # Calcul de la position du rectangle
 rectangle_x_position = 502  # Position x du rectangle
 rectangle_y_position = graduations_positions['1200'][1]  # Base du rectangle au sommet de l'image
@@ -82,13 +96,46 @@ rectangle = Rectangle((rectangle_x_position, rectangle_y_position), w, rectangle
 plt.gca().add_patch(rectangle)
 
 # Affichage de la valeur aléatoire à une position fixe
-random_text = plt.text(150, 900, f'{random_value}m', fontsize=15, ha='left', va='top', color='white')
+random_text = plt.text(-200, 1250, f'{random_value}m', fontsize=15, ha='left', va='top', color='white')
 
 # Mise à jour de la valeur aléatoire chaque seconde
 while True:
+    if can_black_icon :
+        can_black_icon.remove()
+    if atomium_color :
+        atomium_color.remove()
+    if eiffel_tower :
+        eiffel_tower.remove()
+    if burj_khalifa :
+        burj_khalifa.remove()
     random_value = random.randint(0, 1200)
+    if random_value < 102:
+        a = 0
+        e = 0
+        b = 0
+    else:
+        if random_value < 330:
+            a = 1
+            e = 0
+            b = 0
+        else:
+            if random_value < 830:
+                a = 1
+                e = 1
+                b = 0
+            else:
+                a = 1
+                e = 1
+                b = 1
+    atomium_color = add_image_with_position(img_path_atomium_color, atomium_color_position, zorder=a)
+    eiffel_tower = add_image_with_position(img_path_eiffel_tower, eiffel_tower_position, zorder=e)
+    burj_khalifa = add_image_with_position(img_path_burj_khalifa, burj_khalifa_position, zorder=b)
     rectangle_height = graduation_distance * abs(random_value - 1200)
     rectangle_height = (-(1200-random_value) / 1200 * 1170)
     rectangle.set_height(rectangle_height)
+
+    can_black_icon_position = (100, 1810 + rectangle_height)  # Définissez la position
+    can_black_icon = add_image_with_position(img_path_can_black_icon, can_black_icon_position, zorder=3)
+
     random_text.set_text(f'{random_value}m')
     plt.pause(1)
